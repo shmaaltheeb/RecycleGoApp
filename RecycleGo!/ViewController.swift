0e437cb9643d50d7eab213f217875fd8a70ec661
shmaaltheeb <70481872+shmaaltheeb@users.noreply.github.com>
2023-02-19 13:40:04 +0400

    Address Menu added!!

--- RecycleGo!/ViewController.swift
+++ RecycleGo!/ViewController.swift
@@ -7,14 +7,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class CAddress: UIViewController{
+    
+    @IBOutlet var picker1: UIPickerView!
+    
+    let emirates = ["Abu Dhabi","Dubai","Sharjah", "Um AlQuwain","Ras AlKhaimah","Fujairah","Ajman"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "Bar Items"
+        picker1.dataSource = self
+        picker1.delegate = self
+        
+    }
+}
+extension CAddress: UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return emirates.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return emirates[row]
+    }
+}
+
+class ViewController: UIViewController{
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
     }
 
-
 }
+
 
